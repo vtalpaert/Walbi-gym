2e78a7ca0f20d5b68c58d585372b0dbfa6250f25
Victor Talpaert <victor.talpaert@akka.eu>
2019-07-03 17:59:02 +0200

    clean HardSerial example

--- arduino-board/lib/Walbi/examples/WalbiHardSerial/WalbiHardSerial.ino
+++ arduino-board/lib/Walbi/examples/WalbiHardSerial/WalbiHardSerial.ino
@@ -1,8 +1,6 @@
 #include "Walbi.h"
 
 #define SERIAL_BAUD 115200  // Baudrate to PC
-#define DEBUG_BOARD_TX 10  // Arduino soft RX (connects to debug board TX)
-#define DEBUG_BOARD_RX 11  // Arduino soft TX (debub board RX)
 
 walbi_ns::Walbi* walbi;
 
