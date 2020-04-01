1215658d62719d3d8d1282276b856782d496ca11
Victor Talpaert <victor.talpaert@akka.eu>
2020-04-01 19:14:28 +0200

    walk animation

--- walbi/walbi_gym/inverse_kinematic.py
+++ walbi/walbi_gym/inverse_kinematic.py
@@ -2,7 +2,7 @@ import math as m
 import numpy as np
 
 
-class JointsAngleCalculator(object):
+class Walker(object):
 
     """Calculates joints angle specifically for Walbi robot
     
@@ -30,3 +30,59 @@ class JointsAngleCalculator(object):
         RK = m.pi - m.acos((l_upper**2+l_lower**2-z2**2)/(2.*l_upper*l_lower))
         RFY = -RHY-RK
         return np.array([RHX, RHY, RK, RFY, -RHX])
+
+    def circlesXZ(self, t, freq = 0.05):
+        x = 0. + 0.03 * m.sin(2*m.pi*freq*t)
+        y = -0.05
+        z = -0.15 + 0.04 * m.cos(2*m.pi*freq*t)
+        action = np.empty(10, dtype=float)
+        action[:5] = calculator.calculate_joints_angle(x, y, z)  # right
+        action[5:] = calculator.calculate_joints_angle(x, -y, z)  # left
+        return action
+
+    def tiptapsYZ(self, t, freq = 0.1):
+        x = 0
+        y = 0.04 * m.sin(2*m.pi*freq*t)
+        z = -0.17
+        z_up_right = max(0, 0.07 * m.sin(2*m.pi*freq*t + m.pi) - 0.05)
+        z_up_left = max(0, 0.07 * m.sin(2*m.pi*freq*t) - 0.05)
+        action = np.empty(10, dtype=float)
+        action[:5] = calculator.calculate_joints_angle(x, y, z + z_up_right)  # right
+        action[5:] = calculator.calculate_joints_angle(x, y, z + z_up_left)  # left
+        return action
+
+    def walk_one_period(self, p, delta_x):
+        amplitude_y = 0.05
+        amplitude_z = 0.03
+        z = -0.17  # base height
+
+        x = 0
+        y = amplitude_y * m.sin(2*m.pi*p)
+        z_up_left = 0
+        z_up_right = 0
+        rate = -2. / 0.9
+        if p <= 0.2:
+            x_left = delta_x * (-1 + rate * (p - 0.2))
+            x_right = delta_x * (-1 + rate * (p - 0.7))
+        elif 0.2 < p < 0.3:  # left up
+            z_up_left = amplitude_z * 0.5 * (1 - m.cos(2*m.pi*(p-0.2)/(0.1)))
+            x_left = - delta_x * m.cos(m.pi*(p-0.2)/(0.1))
+            x_right = delta_x * (-1 + rate * (p - 0.7))
+        elif 0.3 <= p <= 0.7:
+            x_left = delta_x * (1 + rate * (p - 0.3))
+            x_right = delta_x * (-1 + rate * (p - 0.7))
+        elif 0.7 < p < 0.8:  # right up
+            z_up_right = amplitude_z * 0.5 * (1 - m.cos(2*m.pi*(p-0.7)/(0.1)))
+            x_left = delta_x * (1 + rate * (p - 0.3))
+            x_right = - delta_x * m.cos(m.pi*(p-0.7)/(0.1))
+        else:
+            x_left = delta_x * (1 + rate * (p - 0.3))
+            x_right = delta_x * (1 + rate * (p - 0.8))
+        action = np.empty(10, dtype=float)
+        action[:5] = calculator.calculate_joints_angle(x_right, y, z + z_up_right)  # right
+        action[5:] = calculator.calculate_joints_angle(x_left, y, z + z_up_left)  # left
+        return action
+
+    def walk(self, t, freq = 0.5, delta_x = 0.07):
+        p = (freq*t) % 1
+        return self.walk_one_period(p, delta_x)
